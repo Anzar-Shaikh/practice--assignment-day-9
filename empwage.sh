6e4198812e0f33013eee736587d54ce8699e0381
Anzar-Shaikh <anzarshaikh373@gmail.com>
2021-05-30 09:50:54 +0530

    prese/absent check done

--- empwage.sh
+++ empwage.sh
@@ -3,3 +3,16 @@
 
 echo "Wlecome to Employ Waege Computation Problem."
 
+absent=0
+present=1
+
+check=$((RANDOM%2))
+
+if [ $check -eq 0 ]
+then
+echo "employe is absent."
+
+elif [ $check -eq 1 ]
+then
+echo "employe is present."
+fi
